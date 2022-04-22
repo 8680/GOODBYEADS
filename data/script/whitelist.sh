bf2ed8ca966740bab738f2a3617d6b5796cc2b4b
8680 <85273405+8680@users.noreply.github.com>
2022-04-22 10:58:39 +0800

    Update whitelist.sh

--- data/script/whitelist.sh
+++ data/script/whitelist.sh
@@ -1,4 +1,4 @@
 #网易验证
-sed -i 's/0.0.0.0 cstaticdun.126.net/@@||cstaticdun.126.net^/g' ./rules.txt
+sed -i 's/||cstaticdun.126.net^/@@||cstaticdun.126.net^/g' ./rules.txt
 #QQ音乐PC端登录
-sed -i 's/0.0.0.0 c.y.qq.com/@@||c.y.qq.com^/g' ./rules.txt
+sed -i 's/||c.y.qq.com^/@@||c.y.qq.com^/g' ./rules.txt
