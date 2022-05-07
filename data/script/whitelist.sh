8ca8fc1ef65a696771817b7f314ebe850f84b6a5
8680 <85273405+8680@users.noreply.github.com>
2022-05-07 13:57:39 +0800

    Update whitelist.sh

--- data/script/whitelist.sh
+++ data/script/whitelist.sh
@@ -6,3 +6,5 @@ sed -i 's/||c.y.qq.com^/@@||c.y.qq.com^/g' ./rules.txt
 sed -i 's/||tracker.ai.xiaomi.com^/@@||tracker.ai.xiaomi.com^/g' ./rules.txt
 #阿里验证
 sed -i 's/||acs.m.taobao.com^/@@||acs.m.taobao.com^/g' ./rules.txt
+#淘宝特卖网
+sed -i 's/||gma.alicdn.com^/@@||gma.alicdn.com^/g' ./rules.txt
